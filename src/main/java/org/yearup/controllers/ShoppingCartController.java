8883c9b79fe831d289e7ee86d950e7c2f1727c1a
Tushar Islam <tushar060703@gmail.com>
2026-06-23 16:18:14 -0400

    ShoppingCartController contructor and getCart Method added

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,5 +1,10 @@
 package org.yearup.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.User;
 import org.yearup.service.ShoppingCartService;
@@ -8,16 +13,25 @@ import org.yearup.service.UserService;
 import java.security.Principal;
 
 // convert this class to a REST controller
+@RestController
+@RequestMapping("cart")
+@CrossOrigin
 // only logged in users should have access to these actions
+@PreAuthorize("isAuthenticated()")
+
 public class ShoppingCartController
 {
     // a shopping cart controller depends on the service layer
     private ShoppingCartService shoppingCartService;
     private UserService userService;
 
-
+    public ShoppingCartController(ShoppingCartService shoppingCartService, UserService userService) {
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+    }
 
     // each method in this controller requires a Principal object as a parameter
+    @GetMapping("")
     public ShoppingCart getCart(Principal principal)
     {
         // get the currently logged in username
@@ -27,7 +41,7 @@ public class ShoppingCartController
         int userId = user.getId();
 
         // use the shoppingCartService to get all items in the cart and return the cart
-        return null;
+        return shoppingCartService.getByUserId(userId);
     }
 
     // add a POST method to add a product to the cart - the url should be
