4423cea999fdf19752a2dd8558030b2b5feebca8
Tushar Islam <tushar060703@gmail.com>
2026-06-24 15:49:29 -0400

    Phase 5 started, Order Class created

---
+++ src/main/java/org/yearup/models/Order.java
@@ -0,0 +1,93 @@
+package org.yearup.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private int orderId;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "date")
+    private LocalDateTime date;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "shipping_amount")
+    private double shippingAmount;
+
+    public Order()
+    {
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public double getShippingAmount() {
+        return shippingAmount;
+    }
+
+    public void setShippingAmount(double shippingAmount) {
+        this.shippingAmount = shippingAmount;
+    }
+}
